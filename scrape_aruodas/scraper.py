0976a844d14085bb183ac81eaca24d517031dcc1
Valdas <57098027+valdas-v1@users.noreply.github.com>
2021-01-28 20:55:15 +0200

    Update scraper.py

--- scrape_aruodas/scraper.py
+++ scrape_aruodas/scraper.py
@@ -10,13 +10,13 @@ import time
 
 class Scraper:
     def __init__(self):
+        # Placeholder DataFrame to hold scraped data
         self.df = pd.DataFrame()
 
     def get_soup(self, url: str) -> BeautifulSoup:
-        """
-        Returns BeautifulSoup object of the URL
+        """Returns BeautifulSoup object of the URL
 
-        Parameters:
+        Args:
             url (str): The URL of the page
 
         Returns:
@@ -37,49 +37,17 @@ class Scraper:
         else:
             return BeautifulSoup(response.text, "html.parser")
 
-    def encode_data(self):
-        self.df = self.df.astype(
-            {
-                "Area": "float64",
-                "Build year": "int64",
-                "Floor": "int64",
-                "No. of floors": "int64",
-                "Renovation year": "int64",
-                "price": "float64",
-                "Number of rooms ": "int64",
-            }
-        )
-
-        self.encoded_df = self.df[
-            [
-                "Area",
-                "Build year",
-                "Floor",
-                "No. of floors",
-                "Number of rooms ",
-                "Renovation year",
-            ]
-        ]
-
-        categorical_values = pd.get_dummies(
-            self.df[
-                [
-                    "Building type",
-                    "Equipment",
-                    "Heating system",
-                    "city",
-                    "region",
-                    "street",
-                ]
-            ]
-        )
-
-        self.encoded_df = self.encoded_df.join(categorical_values)
-        pickle.dump(self.encoded_df, open("encoded.pkl", "wb"))
-
     def extract_info(self, soup: BeautifulSoup):
+        """Takes BeautifulSoup object and extracts aparment data. Appends the extracted data to the main DataFrame
+
+        Args:
+            soup (BeautifulSoup): Beautiful soup object of the aparment listing
+        """
+
+        # Aparment data is listed in a dl
         info = soup.find_all("dl")
 
+        # Placeholder DataFrame to hold aparment data. Matches data tags with data
         flat_info = pd.DataFrame()
         cleaned_key = []
         for i in info[0].find_all("dt"):
@@ -94,6 +62,7 @@ class Scraper:
 
         clean_df = flat_info.pivot_table(values="value", columns="key", aggfunc="first")
 
+        # Setting a filter for the desired data
         required_data = [
             "Area",
             "Build year",
@@ -107,11 +76,13 @@ class Scraper:
 
         clean_df = clean_df.filter(required_data)
 
+        # Cleaning up data with regex
         clean_df["Area"] = clean_df["Area"].str.findall(r"(\d+)")
         clean_df["Area"] = ".".join(clean_df["Area"].iloc[0])
 
         clean_df["Build year"] = clean_df["Build year"].str.findall(r"(\d+)")
 
+        # Cheking if aparment has been renovated, if not, setting the renovation date to 0
         clean_df["Renovation year"] = 0
         if len(clean_df["Build year"].iloc[0]) == 2:
             clean_df["Renovation year"].iloc[0] = clean_df["Build year"].iloc[0][1]
@@ -119,10 +90,12 @@ class Scraper:
         else:
             clean_df["Build year"].iloc[0] = clean_df["Build year"].iloc[0][0]
 
+        # Extracting the price
         price = soup.find("span", class_="main-price")
         price = re.findall("\d+", price.text)
         clean_df["price"] = "".join(price)
 
+        # Extracting and parsing the address
         address = soup.find("h1")
         address = address.text.split(",")
 
@@ -137,6 +110,11 @@ class Scraper:
         self.df = self.df.append(clean_df, ignore_index=True)
 
     def scrape_aruodas(self, pages: int):
+        """Iterates through search results, scraping every listing in the page
+
+        Args:
+            pages (int): How many search pages to scrape (there are 25 listings on each page)
+        """
         for page in range(pages):
             search = self.get_soup(f"https://m.en.aruodas.lt/butai/puslapis/{page}/")
             search = search.find_all("a", class_="object-image-link")
@@ -147,5 +125,12 @@ class Scraper:
 
                 self.extract_info(flat)
 
-    def save(self):
-        pickle.dump(self.df, open("df.pkl", "wb"))
+    def save(self, df: DataFrame, name: str):
+        """Saves the provided DataFrame to csv with the specified name
+
+        Args:
+            df (DataFrame): DataFrame to be saved to csv
+            name (str): Name of the csv
+        """
+
+        df.to_csv(f"{name}.csv")
