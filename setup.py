7abd3beee07f6875c84a53580e2671e7fa646f17
Valdas <57098027+valdas-v1@users.noreply.github.com>
2021-01-28 18:19:07 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrape_aruodas",
+    version="0.0.1",
+    author="valdas-v1",
+    description="Collect data from scraping Aruodas.lt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
